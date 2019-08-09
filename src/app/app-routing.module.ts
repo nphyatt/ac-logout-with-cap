8b22cf648473492f040270f9d61b6b77c5abf287
Ken Sodemann <ken@ionic.io>
2019-08-08 20:35:10 -0500

    chore(): upgrade libraries

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
-  { path: 'edit-tea-category', loadChildren: './edit-tea-category/edit-tea-category.module#EditTeaCategoryPageModule' }
+  { path: '', loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule) },
+  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule) },
+  {
+    path: 'edit-tea-category',
+    loadChildren: () => import('./edit-tea-category/edit-tea-category.module').then(m => m.EditTeaCategoryPageModule)
+  }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
