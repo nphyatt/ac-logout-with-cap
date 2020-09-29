c6f9d711946b4ebbf99b229feb1e7d19cff847e1
Ken Sodemann <ken@ionic.io>
2020-09-29 09:16:00 -0500

    chore(): upgrade dependencies

--- src/app/about/about.page.ts
+++ src/app/about/about.page.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthMode } from '@ionic-enterprise/identity-vault';
+
+import { dependencies } from '../../../package.json';
 
 import { AuthenticationService, VaultService } from '@app/services';
 import { User } from '@app/models';
@@ -9,7 +11,13 @@ import { User } from '@app/models';
   templateUrl: 'about.page.html',
   styleUrls: ['about.page.scss'],
 })
-export class AboutPage {
+export class AboutPage implements OnInit {
+  angularVersion: string;
+  authConnectVersion: string;
+  capacitorVersion: string;
+  frameworkVersion: string;
+  identityVaultVersion: string;
+
   user: User;
   authMode: string;
   bioType: string;
@@ -23,6 +31,23 @@ export class AboutPage {
     this.user = await this.authentication.getUserInfo();
     this.authMode = AuthMode[await this.vaultService.getAuthMode()];
     this.bioType = await this.vaultService.supportedBiometricTypes();
+  }
+
+  ngOnInit() {
+    const verSpec = /[\^~]/;
+    this.angularVersion = dependencies['@angular/core'].replace(verSpec, '');
+    this.authConnectVersion = dependencies['@ionic-enterprise/auth'].replace(
+      verSpec,
+      '',
+    );
+    this.capacitorVersion = dependencies['@capacitor/core'].replace(
+      verSpec,
+      '',
+    );
+    this.frameworkVersion = dependencies['@ionic/angular'].replace(verSpec, '');
+    this.identityVaultVersion = dependencies[
+      '@ionic-enterprise/identity-vault'
+    ].replace(verSpec, '');
   }
 
   logout() {
